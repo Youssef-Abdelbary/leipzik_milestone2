ee205633c72246dbc7635abb68d336558e951ee5
Ahmed Hazem <ahazem1612@gmail.com>
2026-06-10 16:30:11 +0200

    Merge login into dev

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
       setError("Please fill in all fields");
       return;
     }
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("http://localhost:5001/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
